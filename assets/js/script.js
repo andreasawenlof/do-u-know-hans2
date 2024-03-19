309556e5cec94eec65f1e9ea337f310b4bdb12b0
mezter1337 <andreas.javert@gmail.com>
2024-03-19 05:41:44 +0100

    Add simple object structure of questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+const quizQuestions = [
+    {
+        question: 'Which Movie? (Interstellar)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Gladiator)',
+        choices: {
+            a: 'Pirates of the Caribbean',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Dune)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Batman)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Lion King)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Cool Runnings)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Sherlock Holmes)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Rush)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Pirates of the Caribbean)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Dunkirk)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Man of Steel)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which Movie? (Inception)',
+        choices: {
+            a: 'Interstellar',
+            b: 'Inception',
+            c: 'Dune'
+        },
+        answer: 'a'
+    }
+];
